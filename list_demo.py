7ef1940715eb5017ff09983b8385d301f0e09010
liyongyong <liyongyong@yingmi.cn>
2024-07-21 16:54:09 +0800

    列表操作

---
+++ list_demo.py
@@ -0,0 +1,6 @@
+a_list = ['abc', 'xyz']
+print(a_list)  # ['abc', 'xyz']
+a_list.append('X')  # a_list 增加一个元素 X
+print(a_list)  # ['abc', 'xyz', 'X']
+a_list.remove('xyz')  # a_list 移除元素 xyz
+print(a_list)  # ['abc', 'X']
