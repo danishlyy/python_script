c769c06ce79b9990c81cd980b7b2012044630394
liyongyong <liyongyong@yingmi.cn>
2024-07-24 17:21:46 +0800

    class 继承

--- user_class_demo.py
+++ user_class_demo.py
@@ -7,13 +7,31 @@ class User():  # 定义一个类
     def print_role(self):  # 定义一个方法
         print('%s : %s %s' % (self.__name, self.age, self.occupation))
 
-    def update_name(self,new_name):
+    def update_name(self, new_name):
         self.name = new_name
-
 
 
 class Monster():
     '定义怪物'
+
+    def __init__(self, age=100):
+        self.age = age
+
+    def run(self):
+        print('移动一个位置')
+
+    pass
+
+
+class Animals(Monster):
+    '定义一个普通怪物'
+
+    def __init__(self, age=10):
+        self.age = age
+
+
+class Boss(Monster):
+    '定义一个超级怪物'
     pass
 
 
@@ -23,7 +41,14 @@ user1.print_role()  # Tom : 18 teacher
 user2.print_role()  # Mike : 20 player
 
 user1.update_name('Peter')
-user1.print_role() # Peter : 18 teacher
+user1.print_role()  # Peter : 18 teacher
 user1.name = 'Jack'
-user1.print_role() # __name 私有 只能通过方法修改，不可以直接改变属性值 Tom : 18 teacher
+user1.print_role()  # __name 私有 只能通过方法修改，不可以直接改变属性值 Tom : 18 teacher
 
+a0 = Monster(30)
+print(a0.age)  # 30
+a0.run()  # 移动一个位置
+
+animals1 = Animals(20)
+animals1.run()  # 移动一个位置
+print(animals1.age)  # 20
